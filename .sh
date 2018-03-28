183548037b3edd39e7dac77dc914eb3f94c157d2
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-03-27 21:58:08 -0700

    Update .sh

--- .sh
+++ .sh
@@ -11,12 +11,6 @@ echo "2 3 5">>file2.txt
 echo "define the difference between file1.txt and file2.txt"; echo ""; diff file1.txt file2.txt; sleep 2; echo "";
 echo "copy file1.txt into file2.txt"; echo ""; cp file1.txt file2.txt; sleep 2; echo "";
 echo "list the contents of directory"; echo ""; ls; sleep 2; echo "";
-
-#hi 
-echo "world"; echo""; touch file; sleep 3; echo "";
-
-echo "create a file"; echo ""; touch file; sleep 2; echo "";
-
 # send hello to /dev/null 
 echo hello > /dev/null
 # create a partition and filesystem
@@ -28,7 +22,6 @@ mount; df -h
 sudo umount /mnt/tmp
 sudo mount /mnt/tmp -remount
 sudo fsck /mnt/tmp
-
 # create directories inode contains a list of filenames and then link to other inodes 
 mkdir dir1 dir2 
 echo a > dir1/file1
@@ -36,10 +29,8 @@ echo b > dir1/file2
 echo c > dir2/fie3
 echo d > dir2/file4
 ln dir1/file1 dir2/file5
-
 #View inode 
 ls -iR dir 
-
 echo "check on kernel system log file and dmesg"; echo ""; less /var/log/kernel.log; sleep 2; echo ""; dmesg | less; sleep 2; echo "";
 echo "Check system's runlevel"; echo ""; who -r; sleep 2; echo"";
 echo "change direcotry"; echo ""; cd /etc/systemd; sleep 2; echo "";
